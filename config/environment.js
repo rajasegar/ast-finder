6221ca5bafd610f8cce6562037f533b658c6cb70
Rajasegar Chandran <rajasegar.c@gmail.com>
2019-11-24 18:55:53 +0530

    [CHORE] Setting up gh-pages

--- config/environment.js
+++ config/environment.js
@@ -45,6 +45,8 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     // here you can enable a production-specific feature
+    ENV.rootURL = '/ast-finder/';
+    ENV.locationType = 'hash';
   }
 
   return ENV;
