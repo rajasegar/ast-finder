7cbcbf0af0d3ebdfc803551ea07fe8bb27cac269
Rajasegar <rajasegarc@freshdesk.com>
2019-11-24 18:52:05 +0530

    [CHORE] Some cleanup and refactoring

--- app/components/ast-finder.js
+++ app/components/ast-finder.js
@@ -7,127 +7,17 @@ import { inject as service } from '@ember/service';
 const j = types.builders; // eslint-disable-line
 
 // Sample code to test
-const _code1 = `let a = 1;
-let b = 'hello';
-let c = false;
-const d = 2;
-var e = true;
-import { foo as bar } from 'lib';
-hello(1, 'world', true, a);
-this.hello(1, 'world', true, a);
-hello.world(1, 'foo', true, a);
+const _code1 = `
+hello();
 foo.bar.baz();
-foo.bar.bax.baz(1, 'foo', true, a);
-if(a === 1) {
-console.log('true');
-foo.bar();
-} else {
-console.log('false');
-foo.baz();
-}
-
-let a = {
-name: 'raja',
-age: 35,
-action: hello()
-};
-
-export default class MyComponent extends ReactComponent {}
-class MyComponent extends ReactComponent {
-  constructor(a,b) {
-    this.a = a;
-    this.b = b;
-  }
-
-  hello(x,y) {
-    console.log(x,y);
-  }
-}
-
-function init() {
-this._super(...arguments);
-}
-
-module('Unit | Utility | codeshift-api', function() {
-
-  let a = 1;
-
-  test('it works', function(assert) {
-    let result = codeshiftApi();
-    assert.ok(result);
-  });
-});
-
-let f = [1, "hello", true, 0, -1];
-let a = () => { console.log('hello') }
-let a = () => console.log('hello')
-let a = () => log('hello')
-let a = () => 2
-
-let { name, age } = a; 
-let a = [1,2,3];
-let [x,y,z] = a;
-this.a = a;
-this.b = 10;
-
-export default class MyComponent extends ReactComponent {
-  constructor(a,b) {
-    this.a = a;
-    this.b = b;
-  }
-
-  hello(x,y) {
-    console.log(x,y);
-  }
-}
-
-expect(find(cfPage.fieldPositionOne).textContent.trim()).to.be.contains(fieldOrder[0]);
-switch(a) {
-  case "hello":
-    console.log("hello");
-    break;
-  default:
-    break;
-}
-
-try {
-  hello();
-} catch(ex) {
-  foo();
-} finally {
-  bar();
-}
-try {
-    throw new Error('oops');
-  }
-  catch (ex) {
-      console.error('inner', ex.message);
-      throw ex;
-    }
-  finally {
-    console.log('finally');
-}
-var text = "";
-
-for (var i = 0; i < 10; i++) {
-  if (i === 3) {
-    continue;
-  }
-  text = text + i;
-}
+let hello = "world";
 `;
 
 const _code = `
-for (var i = 0; i < 10; i++) {
-  if (i === 3) {
-    continue;
-  }
-  text = text + i;
-}
+export default function() {};
 `;
 
 export default Component.extend({
-
 
   customize: service(),
   code: _code,
@@ -148,8 +38,11 @@ export default Component.extend({
         case 'ExpressionStatement':
           return findQuery(node.expression);
 
+        case 'VariableDeclaration':
+          return findQuery(node.declarations[0]);
+
         default:
-          console.log('pseudoAst => ', node.type);
+          console.log('pseudoAst => ', node.type); // eslint-disable-line
           return '';
       }
     });
