20b842d508830bf04167eb8101f0f20266ffc8f8
Rajasegar Chandran <rajasegar.c@gmail.com>
2019-12-06 05:55:02 +0530

    [CHORE] Bump ast-node-finder and show version used

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,9 +1,17 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
+import { computed } from '@ember/object';
+import ENV from 'ast-finder/config/environment';
 
 export default Controller.extend({
 
   customize: service(),
+  emberVersion: computed(function() {
+    return ENV.pkg.devDependencies['ember-source'];
+  }),
+  nodeFinderVersion: computed(function() {
+    return ENV.pkg.dependencies['ast-node-finder'];
+  }),
   actions: {
     toggleDarkMode() {
       this.customize.toggleDarkMode();
